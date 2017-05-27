5c6179ff2d48b0e5391986e73e3e425aac0828ee
Shigeru Kobayashi <kotobuki@gmail.com>
2017-05-27 17:58:13 +0900

    Add johnny-five to add physical input and output

--- recipes/watson_primer/conversation.js
+++ recipes/watson_primer/conversation.js
@@ -28,10 +28,36 @@ var hardware = ['microphone', 'speaker'];
 
 // set up TJBot's configuration
 var tjConfig = {
-    log: {
-        level: 'verbose'
-    }
+  log: {
+    level: 'verbose'
+  },
+  listen: {
+    microphoneDeviceId: 'plughw:1,0',
+    inactivityTimeout: -1, // -1 to never timeout or break the connection. Set this to a value in seconds
+    language: 'en-US' // see TJBot.prototype.languages.listen 
+  }
 };
+
+var five = require('johnny-five');
+var board = new five.Board();
+
+var buttonState = 0;
+var led;
+
+board.on('ready', function() {
+  var button = new five.Button(2);
+  led = new five.Led(3);
+
+  button.on('press', function() {
+    console.log('pressed');
+    buttonState = 1;
+  });
+
+  button.on('release', function() {
+    console.log('released');
+    buttonState = 0;
+  });
+});
 
 // instantiate our TJBot!
 var tj = new TJBot(hardware, tjConfig, credentials);
@@ -45,13 +71,26 @@ console.log("You can also say, \"" + tj.configuration.robot.name + ", tell me a 
 tj.listen(function(msg) {
     // check to see if they are talking to TJBot
     if (msg.startsWith(tj.configuration.robot.name)) {
-        // remove our name from the message
-        var turn = msg.toLowerCase().replace(tj.configuration.robot.name.toLowerCase(), "");
+      // remove our name from the message
+      var turn = {
+        workspace_id: WORKSPACEID,
+        input: {
+          'text': msg.toLowerCase().replace(tj.configuration.robot.name.toLowerCase(), ''),
+          'button': buttonState
+        }
+      };
 
-        // send to the conversation service
-        tj.converse(WORKSPACEID, turn, function(response) {
-            // speak the result
-            tj.speak(response.description);
-        });
+      // send to the conversation service
+      tj._conversation.message(turn, function(err, response) {
+        console.log(response.output);
+        tj.speak(response.output.text.join(' ').trim());
+
+        // Control according to the 'action' specified in the Dialog editor
+        if (response.output.action === 'led_on') {
+          led.fadeIn();
+        } else if (response.output.action === 'led_off') {
+          led.fadeOut();
+        }
+      });
     }
 });
